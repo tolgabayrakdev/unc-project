f4c142c62150aa0a6a6281b719f38019efe1a318
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-12-16 11:49:54 +0300

    s

--- server/api/index.js
+++ server/api/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://unc-project.vercel.app/  ",
+        origin: "https://unc-project.vercel.app",
         methods: ["GET", "POST"],
     }
 });
